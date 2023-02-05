a9483348cedbf8422842b513f7616e30f28a7439
IvanTurHi <Gev465378@yandex.ru>
2023-02-05 22:27:21 +0300

    first

---
+++ src/flask_file.py
@@ -0,0 +1,12 @@
+from flask import Flask
+
+
+def run_flask():
+
+    app = Flask(__name__)
+
+    @app.route('/')
+    def hello_sasha():
+        return 'Hello, Sasha! This is our diploma. CRY'
+
+    app.run(host='82.148.28.79', port=5001)
