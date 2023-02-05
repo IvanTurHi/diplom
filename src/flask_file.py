d2793065db9332d9dec95b1b98c2b1f427f9b49b
IvanTurHi <Gev465378@yandex.ru>
2023-02-05 22:36:11 +0300

    two

--- src/flask_file.py
+++ src/flask_file.py
@@ -7,6 +7,6 @@ def run_flask():
 
     @app.route('/')
     def hello_sasha():
-        return 'Hello, Sasha! This is our diploma. CRY'
+        return 'Hello, Sasha! This is our diploma. CRY!'
 
     app.run(host='82.148.28.79', port=5001)
